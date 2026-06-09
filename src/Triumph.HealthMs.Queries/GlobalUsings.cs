911ba1c384bbca0a5aeffd1c6d995de1e63135f8
Joseph Kyere Antwi <joseph.kyereantwi@heidelbergmaterials.com>
2026-06-09 11:09:22 +0000

    Corrected graphql routing and middleware order

--- src/Triumph.HealthMs.Queries/GlobalUsings.cs
+++ src/Triumph.HealthMs.Queries/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using HotChocolate.Language;
 global using HotChocolate.Resolvers;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
 global using Triumph.HealthMs.Core.CQRS;
 global using Triumph.HealthMs.Core.Features.AppConfigurations.GetAppConfigs;
 global using Triumph.HealthMs.Core.Features.EmployeeManagement.GetAllEmployees;
